531931b4fc0c56f25f132f17afeeea7053996aa7
kuro-1996 <ng.ngocanhtuan96@gmail.com>
2021-05-18 09:13:03 +0700

    feat: create api routes

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-const express = require('express')
+const express = require('express');
+const connectDB = require('./config/db');
 
-const app = express()
+const userRoute = require('./routes/api/user');
+const authRoute = require('./routes/api/auth');
+const profileRoute = require('./routes/api/profile');
+const postRoute = require('./routes/api/post');
 
-const PORT = process.env.PORT || 5000
+const app = express();
 
-app.get('/', (req, res) => res.send('API running'))
+// Connect Database
+connectDB();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API running'));
+
+// Define Routes
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
