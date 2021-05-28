021a10a4a832e4b91d80a06d93cca6f2521b8ad8
kuro-1996 <ng.ngocanhtuan96@gmail.com>
2021-05-28 14:13:43 +0700

    fix deploy

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
 const path = require('path')
+const serverless = require('serverless-http')
 
 const userRoute = require('./routes/api/user');
 const authRoute = require('./routes/api/auth');
@@ -38,4 +39,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-module.exports.handler = app
+module.exports.handler = serverless(app)
