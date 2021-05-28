d220d3f27c7206c6800dcf5afe5053efbce493d3
kuro-1996 <ng.ngocanhtuan96@gmail.com>
2021-05-28 12:56:19 +0700

    fix: add netlify config

--- server/api/server.js
+++ server/api/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('../config/db');
 const cors = require('cors');
 const path = require('path')
 
-const userRoute = require('./routes/api/user');
-const authRoute = require('./routes/api/auth');
-const profileRoute = require('./routes/api/profile');
-const postRoute = require('./routes/api/post');
+const userRoute = require('../routes/api/user');
+const authRoute = require('../routes/api/auth');
+const profileRoute = require('../routes/api/profile');
+const postRoute = require('../routes/api/post');
 
 const app = express();
 
@@ -37,3 +37,5 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports.handler = app
