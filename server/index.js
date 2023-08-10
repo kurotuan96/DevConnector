0f6999fadc1d61feea9a2d3da9d165b23feffb33
kuro96 <ng.ngocanhtuan96@gmail.com>
2023-08-10 13:54:59 +0700

    fix: fix cors origin

--- server/index.js
+++ server/index.js
@@ -1,29 +1,32 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
+const express = require('express')
+const connectDB = require('./config/db')
+const cors = require('cors')
 
-const userRoute = require('./routes/api/user');
-const authRoute = require('./routes/api/auth');
-const profileRoute = require('./routes/api/profile');
-const postRoute = require('./routes/api/post');
+const userRoute = require('./routes/api/user')
+const authRoute = require('./routes/api/auth')
+const profileRoute = require('./routes/api/profile')
+const postRoute = require('./routes/api/post')
 
-const app = express();
+const app = express()
 
 // Connect Database
-connectDB();
+connectDB()
 
-app.use(cors());
+app.use(
+	cors({
+		origin: 'https://dev-connector-community.netlify.app/',
+	})
+)
 
 // Init Middleware
-app.use(express.json({ extended: false }));
-
+app.use(express.json({ extended: false }))
 
 // Define Routes
-app.use('/api/users', userRoute);
-app.use('/api/auth', authRoute);
-app.use('/api/profile', profileRoute);
-app.use('/api/posts', postRoute);
+app.use('/api/users', userRoute)
+app.use('/api/auth', authRoute)
+app.use('/api/profile', profileRoute)
+app.use('/api/posts', postRoute)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
